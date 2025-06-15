d100f2b7b463a06bd59a65874af735cc012439b5
Ismael Silva <ismaelsilva@icloud.com>
2025-06-15 22:28:59 +1200

    Web working local:pnpm, vercel:npm; Admin works locally, not on vervel

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -2,20 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../../dist/web',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
-      }
-    }
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    }
-  }
+    },
+  },
+  build: {
+    outDir: 'dist', // Make sure this is set to 'dist'
+  },
 });
